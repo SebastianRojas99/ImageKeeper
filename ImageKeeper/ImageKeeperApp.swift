49c87f409610a0c2322816e50a9d00f19ff32d39
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-04-15 23:33:33 -0300

    Initial Commit

---
+++ ImageKeeper/ImageKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageKeeperApp.swift
+//  ImageKeeper
+//
+//  Created by Sebastian Marquez Rojas on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
